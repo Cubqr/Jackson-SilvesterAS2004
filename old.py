1e5b52a508f25e8c25eb5b92186d91c18575d3a6
Cubqr <silj23137@wainuiomatahigh.school.nz>
2025-05-01 10:42:49 +1200

    Create old.py

---
+++ old.py
@@ -0,0 +1,21 @@
+'''
+MY PROG v1
+author: Jackson Silvester
+
+This is just a demo
+'''
+
+# import libraries
+import os
+
+# set variables
+float_decimalNumber = '0.5'
+bool_trueFalse = False
+int_userNum = 0
+
+print('some text')
+
+while boolchoice is True:
+  str_userChoice= input("Please pick A B C D")
+   If str_userChoice == 'A'
+     Print("correct")
